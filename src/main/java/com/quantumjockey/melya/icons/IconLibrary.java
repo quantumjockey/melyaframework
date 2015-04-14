6090e3d7a698f2a2179676824b9f2ae02f4faa28
quantumjockey <nickyd242@gmail.com>
2015-04-14 14:17:49 -0700

    Created additional icons, mostly for image manipulation tasks.

--- src/main/java/com/quantumjockey/melya/icons/IconLibrary.java
+++ src/main/java/com/quantumjockey/melya/icons/IconLibrary.java
@@ -4,10 +4,16 @@ import javafx.scene.image.Image;
 
 /*
  * As of 11/22/2014, images have been taken from the "Silk" icon theme images by Mark James via Wikimedia Commons
- * Links to these include, in order of field definition:
+ * Links to these include, in no particular order:
  * Directory Without Data Icon: http://commons.wikimedia.org/wiki/File:Folder-silk.png
  * Data Image Icon: http://commons.wikimedia.org/wiki/File:Folder_image.png
  * Data Group Icon: http://commons.wikimedia.org/wiki/File:Folder_database.png
+ *
+ * As of 4/14/2015, additional images have been taken from the Gnome project image set via Wikimedia Commons
+ * Links to these include, in no particular order:
+ * Invert/Flip Icon: https://commons.wikimedia.org/wiki/File:View-refresh-2.svg
+ * Rotate Left Icon: https://commons.wikimedia.org/wiki/File:Gnome-object-rotate-left.svg
+ * Rotate Right Icon: https://commons.wikimedia.org/wiki/File:Gnome-object-rotate-right.svg
  *
  * Note: Class.getResourceAsStream() path string arguments must be formatted relative to Sources Root to avoid null value retrieval.
  */
@@ -16,9 +22,14 @@ public class IconLibrary {
 
     /////////// Fields //////////////////////////////////////////////////////////////////////
 
-    private final Image folderIcon = new Image(getClass().getResourceAsStream("/com/quantumjockey/melya/icons/images/empty_folder.png"));
-    private final Image groupIcon = new Image(getClass().getResourceAsStream("/com/quantumjockey/melya/icons/images/image_group.png"));
-    private final Image rootIcon = new Image(getClass().getResourceAsStream("/com/quantumjockey/melya/icons/images/data_group.png"));
+    private final String basePath = "/com/quantumjockey/melya/icons/images/";
+
+    private final Image folderIcon = this.getResourceImage(this.basePath + "empty_folder.png");
+    private final Image groupIcon = this.getResourceImage(this.basePath + "image_group.png");
+    private final Image invertIcon = this.getResourceImage(this.basePath + "invert.png");
+    private final Image rootIcon = this.getResourceImage(this.basePath + "data_group.png");
+    private final Image rotateLeftIcon = this.getResourceImage(this.basePath + "rotate_left.png");
+    private final Image rotateRightIcon = this.getResourceImage(this.basePath + "rotate_right.png");
 
     /////////// Accessors ///////////////////////////////////////////////////////////////////
 
@@ -30,8 +41,26 @@ public class IconLibrary {
         return this.groupIcon;
     }
 
+    public Image getInvertIcon() {
+        return this.invertIcon;
+    }
+
     public Image getRootIcon() {
         return this.rootIcon;
+    }
+
+    public Image getRotateLeftIcon() {
+        return this.rotateLeftIcon;
+    }
+
+    public Image getRotateRightIcon() {
+        return this.rotateRightIcon;
+    }
+
+    /////////// Private Methods /////////////////////////////////////////////////////////////
+
+    private Image getResourceImage(String path) {
+        return new Image(getClass().getResourceAsStream(path));
     }
 
 }
